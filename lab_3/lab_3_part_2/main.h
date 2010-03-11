2aab268f944301e6e69f0b80d0a20f81adefb17c
Apkawa <apkawa@gmail.com>
2010-03-11 21:53:03 +0300

    added main.h

---
+++ lab_3/lab_3_part_2/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <list>
+
+using namespace std;
+
+template <typename T> T randint(T a, T b);
+
+void print_list(list<int> v);
+
+list<int> random_range(int size, int a, int b);
+
+void special_print(list<int> l);
+
+void lab_3_part_2(void);
+
+#endif // MAIN_H
